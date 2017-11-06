8c46bfb0a013c94fcbd48e855143a57d65bd733c
Gabor Lekeny <gabor.lekeny@gmail.com>
2017-11-06 06:29:44 +0100

    React setup (2/5)

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -9,6 +10,8 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+
+    CORS(app)
 
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
